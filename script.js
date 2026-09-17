2d84c1cb569e84f88ad28b980932de5638e92eac
adm1080-stack <adm1080@g.rit.edu>
2026-09-17 10:06:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -353,7 +353,7 @@ function drawBackgroundSky() {
   const gradient = ctx.createLinearGradient(0, 0, 0, window.innerHeight);
   if (settings.mode === "dark") {
     gradient.addColorStop(1, "#50C878");
-    gradient.addColorStop(0, "#00674F");
+    gradient.addColorStop(0, "#50C878");
   } else {
     gradient.addColorStop(1, "#F8BA85");
     gradient.addColorStop(0, "#FFC28E");
